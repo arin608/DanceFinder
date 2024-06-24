d937648b59f7cd3d031e13241caa9c6d16434f5e
arin608 <jenna3321@naver.com>
2024-06-24 16:23:46 +0900

    react connect error

--- dancefinder/src/main/java/com/dancefinder/controller/DanceFinderController.java
+++ dancefinder/src/main/java/com/dancefinder/controller/DanceFinderController.java
@@ -4,6 +4,8 @@ package com.dancefinder.controller;
 import com.dancefinder.model.SearchResult;
 import com.dancefinder.service.DanceFinderService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -18,6 +20,10 @@ public class DanceFinderController {
     private final List<SearchResult> searchHistory = new ArrayList<>();
 
     @PostMapping("/search")
+//    public ResponseEntity<?> searchYouTube(@RequestBody SearchQuery searchQuery) {
+//    	return ResponseEntity.ok().body("Search result");
+//    }
+    
     public List<SearchResult> searchDanceVideos(@RequestParam String title, @RequestParam String artist) {
         List<SearchResult> results = danceFinderService.findDanceVideos(title, artist);
         searchHistory.addAll(results); // 검색 결과를 기록에 추가
