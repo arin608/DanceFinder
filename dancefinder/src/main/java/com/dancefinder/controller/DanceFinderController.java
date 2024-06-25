ab503dbb30dfa4c4c27a52037c35b1a6bc71fd72
arin608 <jenna3321@naver.com>
2024-06-25 15:39:01 +0900

    connect success

--- dancefinder/src/main/java/com/dancefinder/controller/DanceFinderController.java
+++ dancefinder/src/main/java/com/dancefinder/controller/DanceFinderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/danceFinder")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class DanceFinderController {
 
     private final DanceFinderService danceFinderService;
